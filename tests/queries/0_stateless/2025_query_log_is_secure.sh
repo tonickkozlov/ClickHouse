7331dc9ef4e8c29b891d0ba4a2011fa545e9e05a
Sean Haynes <shaynes@cloudflare.com>
2022-02-21 07:53:01 +0000

    Fix query_log_is_secure test
    Ok. Isn't returned in stdout, and use a structured query_id for all queries for consistency

--- tests/queries/0_stateless/2025_query_log_is_secure.sh
+++ tests/queries/0_stateless/2025_query_log_is_secure.sh
@@ -4,21 +4,29 @@ CURDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 # shellcheck source=../shell_config.sh
 . "$CURDIR"/../shell_config.sh
 
-${CLICKHOUSE_CLIENT_SECURE} --log_queries=1 --query_id "2025_${CLICKHOUSE_DATABASE}_query_1" -q "select 1"
-${CLICKHOUSE_CLIENT} -q "system flush logs"
-${CLICKHOUSE_CLIENT} -q "select is_secure from system.query_log where query_id = '2025_${CLICKHOUSE_DATABASE}_query_1' and type = 'QueryFinish' and current_database = currentDatabase()"
+QUERY_ID=$(${CLICKHOUSE_CLIENT} -q "select lower(hex(reverse(reinterpretAsString(generateUUIDv4()))))")
 
-QUERY_ID=$RANDOM
-
-${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL_HTTPS}&query_id=${QUERY_ID}" -d "SELECT 1"
+# Native secure
+${CLICKHOUSE_CLIENT_SECURE} --log_queries=1 --query_id "${QUERY_ID}" -q "SELECT 1"
 ${CLICKHOUSE_CLIENT} -q "system flush logs"
 ${CLICKHOUSE_CLIENT} -q "select is_secure from system.query_log where query_id = '${QUERY_ID}' and type = 'QueryFinish' and current_database = currentDatabase()"
 
-${CLICKHOUSE_CLIENT} --log_queries=1 --query_id "2025_${CLICKHOUSE_DATABASE}_query_2" -q "select 1"
-${CLICKHOUSE_CLIENT} -q "system flush logs"
-${CLICKHOUSE_CLIENT} -q "select is_secure from system.query_log where query_id = '2025_${CLICKHOUSE_DATABASE}_query_2' and type = 'QueryFinish' and current_database = currentDatabase()"
+# HTTP secure
+QUERY_ID=$(${CLICKHOUSE_CLIENT} -q "select lower(hex(reverse(reinterpretAsString(generateUUIDv4()))))")
 
-QUERY_ID=$RANDOM
+${CLICKHOUSE_CURL} -sS --insecure "${CLICKHOUSE_URL_HTTPS}&query_id=${QUERY_ID}" -d "SELECT 1"
+${CLICKHOUSE_CLIENT} -q "system flush logs"
+${CLICKHOUSE_CLIENT} -q "select is_secure from system.query_log where query_id = '${QUERY_ID}' and type = 'QueryFinish' and current_database = currentDatabase()"
+
+# Native insecure
+QUERY_ID=$(${CLICKHOUSE_CLIENT} -q "select lower(hex(reverse(reinterpretAsString(generateUUIDv4()))))")
+
+${CLICKHOUSE_CLIENT} --log_queries=1 --query_id "${QUERY_ID}" -q "SELECT 1"
+${CLICKHOUSE_CLIENT} -q "system flush logs"
+${CLICKHOUSE_CLIENT} -q "select is_secure from system.query_log where query_id = '${QUERY_ID}' and type = 'QueryFinish' and current_database = currentDatabase()"
+
+# HTTP insecure
+QUERY_ID=$(${CLICKHOUSE_CLIENT} -q "select lower(hex(reverse(reinterpretAsString(generateUUIDv4()))))")
 
 ${CLICKHOUSE_CURL} -sS "${CLICKHOUSE_URL}&query_id=${QUERY_ID}" -d "SELECT 1"
 ${CLICKHOUSE_CLIENT} -q "system flush logs"
